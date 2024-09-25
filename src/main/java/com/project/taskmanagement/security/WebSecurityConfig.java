58ac2e3c0a69eab844ac0170fa358a8b136344ce
Jayant-stack <jayanthalapati@gmail.com>
2024-09-25 15:21:14 -0400

    adding the logic for implementing the service layers

--- src/main/java/com/project/taskmanagement/security/WebSecurityConfig.java
+++ src/main/java/com/project/taskmanagement/security/WebSecurityConfig.java
@@ -1,4 +1,25 @@
 package com.project.taskmanagement.security;
 
-public class WebSecurityConfig {
+
+import com.project.taskmanagement.security.services.UserDetailsServiceImpl;
+import com.project.taskmanagement.utils.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    final String clientUrl = Client.clientUrl;
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+
+
+
 }
--- src/main/java/com/project/taskmanagement/utils/Client.java
+++ src/main/java/com/project/taskmanagement/utils/Client.java
@@ -1,4 +1,5 @@
 package com.project.taskmanagement.utils;
 
 public class Client {
+    public final static String clientUrl = "http://localhost:8000";
 }
